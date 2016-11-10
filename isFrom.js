aab5ccc38a03660646548e0487e7625378d77733
zukelwa20 <makhosandilzukelwa@gmail.com>
2016-11-10 15:43:02 +0200

    completed the exercise

---
+++ isFrom.js
@@ -0,0 +1,6 @@
+var isFrom = function(regNum,location){
+var results = regNum.startsWith(location);
+return results;
+}
+console.log(isFrom("CA 456789","CA"));
+console.log(isFrom("ND 123456","NA"));
---
+++ regCheck.js
@@ -0,0 +1,8 @@
+var regCheck = function(regNum,location){
+var Check = regNum.startsWith(location);
+return Check;
+}
+console.log(regCheck("GP 123456","GP"));
+console.log(regCheck("L 456321","L"));
+console.log(regCheck("EC 789654","EC"));
+console.log(regCheck("MP 456987","MP"));
