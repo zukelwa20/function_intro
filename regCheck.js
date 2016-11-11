91487d7cc30676fab92b38b8ca01d2dba01d3f77
zukelwa20 <makhosandilezukelwa@gmail.com>
2016-11-11 10:06:59 +0200

    corrected the values of results

--- regCheck.js
+++ regCheck.js
@@ -7,4 +7,4 @@ console.log(regCheck("GP 123456","GP"));
 console.log(regCheck("L 456321","L"));
 console.log(regCheck("EC 789654","EC"));
 console.log(regCheck("MP 456987","MP"));
-assert.equal(regCheck("CJ 563114","GP"),false);
+assert.equal(regCheck("GP 563114","GP"),true);
