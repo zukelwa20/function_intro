927feeac9798ee83ed01968ff0445f11228b3c06
zukelwa20 <makhosandilzukelwa@gmail.com>
2016-11-14 15:15:25 +0200

    modified yearsAgo

--- yearsAgo.js
+++ yearsAgo.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
 var yearsAgo = function(year1,year2){
-var y1 =  new Date(year1);
-var y2 = new Date(year2);
-results = Number(y1.getFullYear()-y2.getFullYear());
+  var y1 =  new Date(year1);
+  var y2 = new Date(year2);
+  results = Number(y1.getFullYear()-y2.getFullYear());
 console.log(results);
 return results;
 }
